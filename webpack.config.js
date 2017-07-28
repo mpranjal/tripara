9409e885a6cc7d058f256d75643c552dedb9d6a3
pranjal mishra <pranjal.3636@gmail.com>
2017-07-28 15:36:48 +0530

    added modernizr

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,14 @@
 var webpack = require('webpack');
 
 module.exports = {
-  entry:  "./app/assets/scripts/App.js",
+  entry:  {
+	  App: "./app/assets/scripts/App.js",
+	  Vendor: "./app/assets/scripts/Vendor.js"
+  },
+
+
   output: {
     path: __dirname + "/app/temp/scripts",
-    filename: "App.js"
+    filename: "[name].js"
   }
 };

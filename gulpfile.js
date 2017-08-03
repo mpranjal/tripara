07d2344a9b215bb70c9101018db5d1cab96cf1fb
pranjal mishra <pranjal.3636@gmail.com>
2017-08-03 18:47:05 +0530

    carlist completed

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 require('./gulp/tasks/styles');
 require('./gulp/tasks/watch');
 require('./gulp/tasks/scripts');
-require('./gulp/tasks/modernizr');
-// require('./gulp/tasks/build');
+// require('./gulp/tasks/modernizr');
+require('./gulp/tasks/build');
 
 
 

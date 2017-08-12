6efc1e27db63aa3a3e578c1fe714d0a2f5b90a8a
pranjal mishra <pranjal.3636@gmail.com>
2017-08-12 18:54:23 +0530

    ..

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -27809,14 +27809,14 @@ function GLV()
 	grid.click( function() {
 		  // console.log('ok');
 		  $('main > div').removeClass('col-lg-12');
-		  $('main > div').addClass('col-lg-4');
+		  $('main > div').addClass('col-lg-6');
 		  $('main').removeClass('list');
 		  $('main').addClass('grid');
 	});
 
 	list.click( function() {
 	  // console.log('ok');
-	  $('main > div').removeClass('col-lg-4');
+	  $('main > div').removeClass('col-lg-6');
 	  $('main > div').addClass('col-lg-12');
 	  $('main').removeClass('grid');
 	  $('main').addClass('list');
